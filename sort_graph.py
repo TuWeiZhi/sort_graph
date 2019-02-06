50fead7c568d5df1c0ad1b3306e4947037ea51d7
twz <tuweizhi@126.com>
2019-02-06 15:05:44 +0800

    sort_graph

--- sort_graph.py
+++ sort_graph.py
@@ -1,6 +1,7 @@
 import random
 from matplotlib import pyplot as plt
 import matplotlib.animation as animation
+import numpy as np
 
 def random_list():
     list = []
@@ -25,29 +26,48 @@ def quick_sort(list, low, high):
         quick_sort(list, j + 1, high)
     return list
 
+def bubble_sort():
+    for i in range(len(list)):
+        for j in range(0, len(list) - 1 - i):
+            if list[j] > list[j + 1]:
+                list[j], list[j + 1] = list[j + 1], list[j]
+                yield list
+
 def init():
-    ax.set_xlim(-1, 100)
-    ax.set_ylim(-1, 1000)
-    pass
-# def fast_sort(list):
-#     length = len(list)
-#     result_list = quick_sort(list, 0, length - 1)
+    ax.set_xlim(0, 101)
+    ax.set_ylim(0, 100)
+    return
+
+def update(data):
+    for i in range(len(data)):
+        lines[i].set_ydata([0, data[i]])
+
+    return lines
+
+def quick_sort_temp():
+    result= quick_sort(list, 0, len(list) - 1)
+    yield result
 
 if __name__ == '__main__':
-    plt.figure(figsize=(14, 8))
-    fig, ax = plt.subplot()
-
-    list = random_list()
+    # plt.figure(figsize=(14, 8))
+    fig, ax = plt.subplots()
+    n = 100
+    list = np.random.rand(n)*100
+    # print(list)
+    # print(len(list))
+    # print(type(list))
     # fast_sort_list = fast_sort(list)
     # print(list)
-
+    xdata = [[i+1 for i in range(n)], [i+1 for i in range(n)]]
+    ydata = [[0 for i in range(n)], list]
 # x = [1, 2, 3]
 # y = [6, 8, 10]
 # for i in range(10):
 #     y = [i + 1 for i in y]
 #     print(len(list))
 #     plt.bar([i for i in range(len(list))], list, width=0.5)
+    lines = ax.plot(xdata, ydata)
 
 # fig, ax = plt.subplot()
-    ani = animation.FuncAnimation(fig=fig, init_func=init)
+    ani = animation.FuncAnimation(fig, update, bubble_sort, interval=1, init_func=init)
     plt.show()
